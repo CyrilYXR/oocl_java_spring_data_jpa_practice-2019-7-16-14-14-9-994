df231ac33b6aaa38852899f4cc0e844044bf2b22
CyrilYao <cyril.yao@oocl.com>
2019-07-17 17:56:34 +0800

    [Cyril]: One to one and one to many hands-on

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,30 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
-    
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+
     @GetMapping(produces = {"application/json"})
-    public Iterable<Company> list() {
-        return null;
+    public ResponseEntity list() {
+        List<Company> companies = companyRepository.findAll();
+        return ResponseEntity.ok().body(companies);
     }
     
     @PostMapping(produces = {"application/json"})
-    public Company add(@RequestBody Company company) {
-        return null;
+    public ResponseEntity add(@RequestBody Company company) {
+        Company companySaved = companyRepository.saveAndFlush(company);
+        return ResponseEntity.status(HttpStatus.CREATED).body(companySaved);
     }
 }
--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,9 +1,38 @@
 package com.tw.apistackbase.core;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "id", referencedColumnName = "profile")
+    private CompanyProfile profile;
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Employee> employees;
+
+    public CompanyProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(CompanyProfile profile) {
+        this.profile = profile;
+    }
 
     public Long getId() {
         return id;
